33ffc77ff8b923ef957ea950f322f4f4cdfce14e
Fabian <fabiangabel@hotmail.de>
2013-04-26 22:33:04 +0200

    using binaries for grid_?.out

--- charModule.f90
+++ charModule.f90
@@ -4,6 +4,7 @@ module charModule
     character(len=20) :: FILIN, FILOUT,VTKFILE,&
                         BLOCKFILE,BLOCK_CH,PROCFILE,PROC_CH,&
                         TIME_CH
+    character(len=11) :: FORM_CH='UNFORMATTED'
     integer :: BLOCKUNIT,PROC,PROCUNIT
     integer, parameter :: OFFSET=20
 
