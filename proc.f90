c6ab59e8ae507052471df55186bd14a26fa1da6f
Fabian <fabiangabel@hotmail.de>
2013-06-01 16:30:52 +0200

    bug fix proc.f90

--- proc.f90
+++ proc.f90
@@ -2,7 +2,7 @@ program proc
 
     implicit none
 
-    integer :: NPROCS,NBLOCKS,I,J,IBLOCK
+    integer :: NPROCS,NBLOCKS,I,J,IBLOCK,STRIDE
     character(len=20) :: FILOUT,BLOCKNUMBER,PROCNUMBER
 
     print *, 'ENTER NUMBER OF BLOCKS'
@@ -10,15 +10,19 @@ program proc
     print *, 'ENTER NUMBER OF PROCS'
     read (*,*) NPROCS
 
+    STRIDE=NBLOCKS/NPROCS
+
     IBLOCK=0
     do I=0,NPROCS-1
+        print *, I
         write(PROCNUMBER,*) I
         FILOUT='proc_'//trim(adjustl(PROCNUMBER))//'.inp'
         open(UNIT=20,FILE=FILOUT)
         rewind 20
-        write(20,*) NBLOCKS/NPROCS
-        write(20,*) (J,J=IBLOCK,IBLOCK+NBLOCKS/NPROCS)
-        IBLOCK=IBLOCK+NBLOCKS/NPROCS+1
+        write(20,*) STRIDE
+        print *, IBLOCK,STRIDE,(J,J=IBLOCK+1,IBLOCK+STRIDE)
+        write(20,*) (J,J=IBLOCK,IBLOCK+STRIDE)
+        IBLOCK=IBLOCK+STRIDE
     end do
 
 end program proc
